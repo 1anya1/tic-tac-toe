51313aee41287d0660c92009cea35962ad2e83f7
Anna <filatovaa@yahoo.com>
2020-01-30 23:30:40 -0800

    simple layout complete

--- app.js
+++ app.js
@@ -1,4 +1,57 @@
+
+
+class Header extends React.Component {
+    render() {
+        return (
+            <h1>Tic Tac Toe</h1>
+        )
+    }
+}
+
+class Players extends React.Component {
+    render() {
+        return (
+            <div>
+            <h3>Player {this.props.whichPlayer}</h3>
+            <h4>Wins:</h4>
+            </div>
+        )
+    }
+}
+
+class Board extends React.Component {
+    render() {
+        return (
+            <div>
+                <div>square</div>
+                <div>square</div>
+                <div>square</div>
+
+                <div>square</div>
+                <div>square</div>
+                <div>square</div>
+
+                <div>square</div>
+                <div>square</div>
+                <div>square</div>
+            </div>
+        )
+    }
+}
+class App extends React.Component {
+    render() {
+       return (
+        <div>
+        <Header />
+        <Players  whichPlayer="x"/>
+        <Players whichPlayer="o"/>
+        <Board />
+        </div>
+       )
+    }
+}
+
 ReactDOM.render(
-    <h1>React Tic Tac Toe</h1>,
+    <App />,
     document.querySelector('.container')
 )
